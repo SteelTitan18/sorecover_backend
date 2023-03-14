79f1324f02ed5c86dfb754ba95b4b40e2aa2530e
steeltitanunbrk <fortune18000@gmail.com>
2023-03-14 04:25:18 +0000

    création des classes utilisateurs

--- recovering/models.py
+++ recovering/models.py
@@ -4,4 +4,27 @@ from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
 from datetime import *
 
+
 # Create your models here.
+class Member(User):
+    phone_number = PhoneNumberField(verbose_name="Numéro de téléphone", unique=True)
+    city = models.fields.charField(verbose_name="Ville", max_length=200)
+    neighborhood = models.fields.charField(verbose_name="Quartier", max_length=200)
+
+
+class PremiumMember(Member):
+
+    def action(self):
+        pass
+
+
+class Supervisor(Member):
+
+    def action(self):
+        pass
+
+
+class Admin(Member):
+
+    def action(self):
+        pass
