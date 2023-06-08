3ca3eff8f254a6eef6ac4a95085bba639fd06817
steeltitanunbrk <fortune18000@gmail.com>
2023-06-08 12:25:08 +0000

    route de validation

--- recovering/views.py
+++ recovering/views.py
@@ -255,7 +255,7 @@ class FinalVersionViewSet(ModelViewSet):
 
 class ValidateCommunity(APIView):
 
-    permission_classes = [OnlyAdmin]
+    permission_classes = [IsAdminOrReadOnly]
 
     @renderer_classes((JSONRenderer,))
     @parser_classes([JSONParser])
